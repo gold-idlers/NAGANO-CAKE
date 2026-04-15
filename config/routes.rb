e53a6eaea918f84d07f891691cf33054491c4767
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-04-15 02:04:41 +0000

    Add customer authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # 顧客用
+  get  "/signup", to: "public/registrations#new"
+  post "/signup", to: "public/registrations#create"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  scope module: :public do
+    resource :session, only: [:new, :create, :destroy]
+    resources :passwords, param: :token
+  end
+
+  # 管理者用
+  namespace :admin do
+    resource :session, only: [:new, :create, :destroy]
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
