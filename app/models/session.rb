b7fc018b4073deb064d1c943549baae1b32d0e20
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-04-15 02:28:41 +0000

    Add admin authentication

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,4 @@
 class Session < ApplicationRecord
-  belongs_to :customer
+  belongs_to :customer, optional: true
+  belongs_to :admin, optional: true
 end
--- db/migrate/20260415005738_create_customers.rb
+++ db/migrate/20260415005738_create_customers.rb
@@ -1,11 +1,19 @@
-class CreateUsers < ActiveRecord::Migration[8.0]
+class CreateCustomers < ActiveRecord::Migration[8.0]
   def change
-    create_table :users do |t|
+    create_table :customers do |t|
       t.string :email_address, null: false
       t.string :password_digest, null: false
+      t.string :last_name
+      t.string :first_name
+      t.string :last_name_kana
+      t.string :first_name_kana
+      t.string :postal_code
+      t.string :address
+      t.string :phone_number
+      t.boolean :is_active, default: true, null: false
 
       t.timestamps
     end
-    add_index :users, :email_address, unique: true
+    add_index :customers, :email_address, unique: true
   end
 end
