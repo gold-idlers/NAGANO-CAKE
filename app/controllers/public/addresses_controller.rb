32d185843f61cb629e05da71f18192690e708f28
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-04-16 07:29:28 +0000

    Add controller for addresses

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,41 @@
 class Public::AddressesController < Public::ApplicationController
   def index
-  end
-
-  def edit
+    @addresses = current_customer.addresses
   end
 
   def create
+    @address = current_customer.address.new(address_params)
+    if @address.save
+      redirect_to addresses_path, notice: "配送先を追加しました"
+    else
+      @address = current_customer.address
+      render :index, states: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @address = current_customer.address.find(params[:id])
   end
 
   def update
+    @address = current_customer.addresses.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path, notice: "配送先を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @address = current_customer.addresses.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path, notice: "配送先を削除しました"
   end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
+  end
+
 end
