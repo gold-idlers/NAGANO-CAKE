aa3fb1b92ba1787f45ed1073296277064c244f90
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-04-16 05:45:01 +0000

    Add admin customers show

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,14 @@
 class Admin::CustomersController < Admin::ApplicationController
   def index
-    @customers = Customer.all.page(params[:page])
-
+    @customers = if params[:search].present?
+      Customer.where("email_address LIKE ?", "%#{params[:search]}%")
+    else
+      Customer.all
+    end.page(params[:page])
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
