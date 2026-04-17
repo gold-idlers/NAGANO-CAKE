56330bf8b2f980e6113c7532599023ec2f2a5979
m20ja1 <264754617+m20ja1@users.noreply.github.com>
2026-04-17 08:06:51 +0000

    [Add] item_newly_registration

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,9 +3,18 @@ class Admin::ItemsController < Admin::ApplicationController
   end
 
   def new
+    @item = Item.new
+    @genres = Genre.all
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item), notice: "商品を登録しました"
+    else
+      @genres = Genre.all
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -15,5 +24,18 @@ class Admin::ItemsController < Admin::ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :introduction,
+      :genre_id,
+      :price,
+      :is_active,
+      :image
+    )
   end
 end
