f8c39988e659923592cc08b2c026a45d76605f42
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-04-19 02:18:16 +0000

    Add controller for cart_items

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,16 +1,38 @@
 class Public::CartItemsController < Public::ApplicationController
   def index
+    @cart_items = current_customer.cart_items
   end
 
   def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    if @cart_item.save
+      redirect_to cart_items_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   def update
+    @cart_item = current_customer.cart_items.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to cart_items_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @cart_item = current_customer.cart_items.find(params[:id]).destroy
+    redirect_to cart_items_path
   end
 
   def destroy_all
+    @cart_items = current_customer.cart_items.destroy_all
+    redirect_to cart_items_path
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
