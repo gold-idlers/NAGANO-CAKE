222ac66bfa0de2736036258392adf85ab611aa3c
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-04-21 01:24:20 +0000

    Fix order flow and date format

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,5 +6,4 @@ class Order < ApplicationRecord
   validates :postal_code, presence: true
   validates :address, presence: true
   validates :name, presence: true
-  validates :payment_method, presence: true
 end
