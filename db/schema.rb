1918ef7b98158f56c8c94754a03ee394d83779bc
m20ja1 <264754617+m20ja1@users.noreply.github.com>
2026-04-22 01:45:23 +0000

    [Fix] status

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_04_17_072943) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_22_013803) do
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
+    t.string "content_type"
     t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -40,92 +40,92 @@ ActiveRecord::Schema[8.0].define(version: 2026_04_17_072943) do
   end
 
   create_table "addresses", force: :cascade do |t|
-    t.integer "customer_id"
-    t.string "postal_code"
     t.string "address"
-    t.string "name"
     t.datetime "created_at", null: false
+    t.integer "customer_id"
+    t.string "name"
+    t.string "postal_code"
     t.datetime "updated_at", null: false
   end
 
   create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "email_address"
     t.string "password_digest"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_admins_on_email_address", unique: true
   end
 
   create_table "cart_items", force: :cascade do |t|
-    t.integer "item_id"
-    t.integer "customer_id"
     t.integer "amount"
     t.datetime "created_at", null: false
+    t.integer "customer_id"
+    t.integer "item_id"
     t.datetime "updated_at", null: false
   end
 
   create_table "customers", force: :cascade do |t|
-    t.string "email_address", null: false
-    t.string "password_digest", null: false
-    t.string "last_name"
-    t.string "first_name"
-    t.string "last_name_kana"
-    t.string "first_name_kana"
-    t.string "postal_code"
     t.string "address"
-    t.string "phone_number"
-    t.boolean "is_active", default: true, null: false
     t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "first_name"
+    t.string "first_name_kana"
+    t.boolean "is_active", default: true, null: false
+    t.string "last_name"
+    t.string "last_name_kana"
+    t.string "password_digest", null: false
+    t.string "phone_number"
+    t.string "postal_code"
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_customers_on_email_address", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 
   create_table "items", force: :cascade do |t|
-    t.integer "genre_id"
-    t.string "name"
-    t.text "introduction"
-    t.integer "price"
-    t.boolean "is_active"
     t.datetime "created_at", null: false
+    t.integer "genre_id"
+    t.text "introduction"
+    t.boolean "is_active"
+    t.string "name"
+    t.integer "price"
     t.datetime "updated_at", null: false
   end
 
   create_table "order_details", force: :cascade do |t|
-    t.integer "order_id"
-    t.integer "item_id"
-    t.integer "price"
     t.integer "amount"
-    t.integer "making_status"
     t.datetime "created_at", null: false
+    t.integer "item_id"
+    t.integer "making_status"
+    t.integer "order_id"
+    t.integer "price"
     t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "customer_id"
-    t.string "postal_code"
     t.string "address"
-    t.string "name"
-    t.integer "shopping_cost"
-    t.integer "total_payment"
-    t.integer "payment_method"
-    t.integer "status"
     t.datetime "created_at", null: false
+    t.integer "customer_id"
+    t.string "name"
+    t.integer "payment_method"
+    t.string "postal_code"
+    t.integer "shopping_cost"
+    t.integer "status", default: 0
+    t.integer "total_payment"
     t.datetime "updated_at", null: false
   end
 
   create_table "sessions", force: :cascade do |t|
+    t.integer "admin_id"
+    t.datetime "created_at", null: false
     t.integer "customer_id"
     t.string "ip_address"
-    t.string "user_agent"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "admin_id"
+    t.string "user_agent"
     t.index ["admin_id"], name: "index_sessions_on_admin_id"
     t.index ["customer_id"], name: "index_sessions_on_customer_id"
   end
