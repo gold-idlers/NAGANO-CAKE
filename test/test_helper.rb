aa9eb85914fedac12e3b34b12f824ae8d8de7cf8
m20ja1 <264754617+m20ja1@users.noreply.github.com>
2026-04-23 10:40:42 +0000

    [Add] public_design

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,12 +4,25 @@ require "rails/test_help"
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
+  end
+end
 
-    # Add more helper methods to be used by all tests here...
+module ActionDispatch
+  class IntegrationTest
+    def sign_in_as_customer(customer)
+      post session_path, params: {
+        email_address: customer.email_address,
+        password: "password"
+      }
+    end
+
+    def sign_in_as_admin(admin)
+      post admin_session_path, params: {
+        email_address: admin.email_address,
+        password: "password"
+      }
+    end
   end
 end
