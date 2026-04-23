7919c9538101b8cd2bf4095ed83d4497f728bd98
m20ja1 <264754617+m20ja1@users.noreply.github.com>
2026-04-23 05:30:40 +0000

    [Fix] Name_Error

--- test/controllers/admin/homes_controller_test.rb
+++ test/controllers/admin/homes_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class Admin::HomesControllerTest < ActionDispatch::IntegrationTest
   test "should get top" do
-    get admin_homes_top_url
+    get admin_url
     assert_response :success
   end
 end
